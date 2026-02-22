6931ada9afbbcf8d0cfec38487039b5b123f79a5
araikazuaki0504 <kaba_hipop@iCloud.com>
2026-02-22 22:17:55 +0900

    ipアドレスを設定

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -171,6 +171,6 @@ app.post("/gameEvent", (req: express.Request, res: express.Response) => {
   
 });
 
-app.listen(3000, "133.242.148.242", () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Shogi server listening on :3000");
 });
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "133.242.148.242",
+    host: "0.0.0.",
     watch: {
       usePolling: true,
     },
